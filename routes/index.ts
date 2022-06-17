b82c83c62f3ffb93fd4e311c3c52aed5350ec3ed
EyuelMathewos <31162254+EyuelMathewos@users.noreply.github.com>
2022-06-17 16:43:20 +0300

    Update index.ts

--- routes/index.ts
+++ routes/index.ts
@@ -5,7 +5,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req: Request, res: Response) {
-  res.render('index', { title: 'Express' });
+    res.send("Express is running");
 });
 
 module.exports = router;
