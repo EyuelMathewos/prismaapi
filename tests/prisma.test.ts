962b43aada625e3099159d89811f10870dfe4e01
EyuelMathewos <31162254+EyuelMathewos@users.noreply.github.com>
2022-06-17 17:18:08 +0300

    Delete tests directory

--- tests/prisma.test.ts
+++
@@ -1,9 +0,0 @@
-const { PrismaClient } = require('@prisma/client');
-const prisma = new PrismaClient()
-describe('Prisma client test',()=>{
-    test('test query',async()=>{
-        const data = await prisma.user.findMany({take: 1, select:{id:true}})
-        expect(data).toBeTruthy()
-    })
-
-})
