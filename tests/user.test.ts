bf33215cf1a745291882e05ca3d8a441e798eded
EyuelMathewos <eyuelmathewos@gmail.com>
2022-07-05 14:57:08 +0300

    testing using authenticated user

--- tests/user.test.ts
+++ tests/user.test.ts
@@ -1,14 +1,16 @@
+import { Response } from "superagent";
 import supertest from "supertest";
 const userapi = require("../app");
 const request = supertest(userapi);
-
+let bearerToken: Response;
 
 describe('user status test',()=>{
     beforeAll( async()=>{
-        const response = await request.post("/users").send({
-            name: "eyuelmathewos@gmail.com",
-            password: "12345678"
+            const response = await request.post("/users/login").send({
+            email: "test12@gmail.com",
+            password: "123456"
         });
+        bearerToken = response.body.token
     })
 
     test('validation error check user',async()=>{
@@ -16,23 +18,25 @@ describe('user status test',()=>{
             name:"eyuel@gmail.com",
             password:12345678
         });
+        console.log(bearerToken)
         expect(response.status).toBe(412);
     })
 
-    // test('get user',async()=>{
-    //     const response = await request.get("/users");
-    //     expect(response.status).toBe(200);
-    // })
-
-    test('create user',async()=>{
-        const response = await request.post("/users").send({
-            name:"eyuel@gmail.com",
-            email: "test@gmail.com",
-            password: "1234567",
-            role: "1"
-        });
+    test('get user',async()=>{
+        const response = await request.get("/users")
+        .set('Authorization', `Bearer ${bearerToken}` );
         expect(response.status).toBe(200);
     })
+
+    // test('create user',async()=>{
+    //     const response = await request.post("/users").send({
+    //         name:"eyuel@gmail.com",
+    //         email: "test@gmail.com",
+    //         password: "1234567",
+    //         role: "1"
+    //     });
+    //     expect(response.status).toBe(200);
+    // })
 
 
 })
